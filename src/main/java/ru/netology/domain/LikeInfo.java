1555db1abede4fa7b8bcba8e6caacde007c8faf5
Sasha <an78art@gmail.com>
2021-03-06 19:04:19 +0300

    v.1

--- src/main/java/ru/netology/domain/LikeInfo.java
+++ src/main/java/ru/netology/domain/LikeInfo.java
@@ -3,11 +3,22 @@ package ru.netology.domain;
 public class LikeInfo {
     int count;//число пользователей, которым понравилась запись
     boolean usersLikes;//наличие отметки «Мне нравится» от текущего пользователя
+    boolean canLike;//может ли пользователь поставить лайк
+    boolean canPublish;//может ли пользователь сделать репост
 
     public int getCount() {return count;}
     public void setCount(int count) {this.count = count;}
 
     public boolean isUsersLikes() {return usersLikes;}
     public void setUsersLikes(boolean usersLikes) {this.usersLikes = usersLikes;}
+
+    public boolean isCanLike() {return canLike;}
+    public void setCanLike(boolean canLike) {this.canLike = canLike;}
+
+    public boolean isCanPublish() {return canPublish;}
+    public void setCanPublish(boolean canPublish) {this.canPublish = canPublish;}
+
+
+
 
 }
